8189827fe6183c9995e04de23896c71262c3bd8f
Aayushi-B2 <Aayushi-B2@users.noreply.github.com>
2020-04-17 16:06:20 +0530

    Code file created.

---
+++ loan_analysis_plots/code.py
@@ -0,0 +1,104 @@
+# --------------
+#Importing header files
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+
+#Code starts here
+
+data = pd.read_csv(path)
+
+loan_status = data['Loan_Status'].value_counts()
+
+loan_status.plot(kind = 'bar')
+
+
+
+# --------------
+#Code starts here
+property_and_loan = data.groupby(['Property_Area','Loan_Status']).size().unstack()
+
+property_and_loan.plot(kind = 'bar', stacked = False)
+
+plt.xlabel('Property Area')
+
+plt.ylabel('Loan Status')
+
+plt.xticks(rotation = 45)
+
+plt.show()
+
+
+# --------------
+#Code starts here
+
+education_and_loan = data.groupby(['Education','Loan_Status']).size().unstack()
+
+education_and_loan.plot(kind = 'bar',stacked = True)
+plt.xlabel('Education Status')
+
+plt.ylabel('Loan Status')
+
+plt.xticks(rotation = 45)
+
+
+# --------------
+#Code starts here
+
+graduate = data[data['Education']== 'Graduate']
+
+not_graduate = data[data['Education']== 'Not Graduate']
+
+graduate['LoanAmount'].plot(kind='density', label='Graduate')
+
+not_graduate['LoanAmount'].plot(kind='density', label='Not Graduate')
+
+
+
+
+
+
+
+
+#Code ends here
+
+#For automatic legend display
+plt.legend()
+
+
+# --------------
+#Code starts here
+
+#Setting up the subplots
+fig, (ax_1, ax_2,ax_3) = plt.subplots(1,3, figsize=(20,8))
+
+#Plotting scatter plot
+ax_1.scatter(data['ApplicantIncome'],data["LoanAmount"])
+
+#Setting the subplot axis title
+ax_1.set(title='Applicant Income')
+
+
+#Plotting scatter plot
+ax_2.scatter(data['CoapplicantIncome'],data["LoanAmount"])
+
+#Setting the subplot axis title
+ax_2.set(title='Coapplicant Income')
+
+
+#Creating a new column 'TotalIncome'
+data['TotalIncome']= data['ApplicantIncome']+ data['CoapplicantIncome']
+
+#Plotting scatter plot
+ax_3.scatter(data['TotalIncome'],data["LoanAmount"])
+
+#Setting the subplot axis title
+ax_3.set(title='Total Income')
+
+
+#Code ends here
+
+
